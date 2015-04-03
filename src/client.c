a02c0fc1c1cdf7a623b7c4e9c6956bdead7dd510
vkokkila <ville.kokkila@aalto.fi>
2015-04-03 11:01:08 +0300

    join channel

--- src/client.c
+++ src/client.c
@@ -11,11 +11,20 @@
 #include <ctype.h>
 
 #define MAX_LENGHT 64
+#define SERVER_PORT "13337"
+
+#define COLOR_RED     "\033[22;31m"
+#define COLOR_GREEN   "\033[22;32m"
+#define COLOR_YELLOW  "\033[01;33m"
+#define COLOR_BLUE    "\033[22;34m"
+#define COLOR_MAGENTA "\033[22;35m"
+#define COLOR_CYAN    "\033[22;36m"
+#define COLOR_RESET   "\033[0m"
 
 int tcp_connect(const char *host) {
 	int sockfd, n;
 	char server_addr[80];
-	char *serv = "13337";
+	char *serv = SERVER_PORT;
 	struct addrinfo hints, *res, *ressave;
 
 	memset(&hints, 0, sizeof(struct addrinfo));
@@ -53,7 +62,7 @@ int tcp_connect(const char *host) {
 		sockfd = -1;
 		return 1;
 	} else {
-		printf("###Connected to: %s [%s:%s]\n", host, server_addr, serv);
+		printf("###Connected to: "COLOR_CYAN"%s "COLOR_RESET"[%s]:%s\n", host, server_addr, serv);
 	}
 
 	freeaddrinfo(ressave);
@@ -66,21 +75,86 @@ typedef struct sock_thdata
 	int socket;
 } thdata;
 
+
+typedef struct {
+	int id;
+	char name[16];
+	char messages[10][256];
+} channel_t;
+
+typedef struct {
+	int num_of_channels;
+	channel_t *channels[25];
+} channel_list_t;
+
+void add_channel(channel_list_t *channel_list, channel_t *channel) {
+	channel_list->channels[channel_list->num_of_channels] = channel;
+	channel_list->num_of_channels++;
+}
+
+channel_t *current_channel;
+
 // Read user input and send to server
 void * send_message(void *ptr) {
 	char tx_buff[MAX_LENGHT], line[MAX_LENGHT - 1];
 	thdata *data;
 	data = (thdata *) ptr;
+	
+	channel_list_t channel_list;
+	channel_list.num_of_channels = 0;
 	//printf("give commands\n");
 	for(;;) {
-		scanf(" %[0-9a-zA-ZöÖäÄåÅ ]", line);
+		memset(line, '\0', sizeof(line));
+		memset(tx_buff, '\0', sizeof(tx_buff));
+		scanf(" %[0-9a-zA-ZöÖäÄåÅ!#%&?()/.,:; ]", line);
 		strcpy(tx_buff, line);
 		strcat(tx_buff, "\n");
 		if (strcmp(line, "") != 0) {
+			
+			char command_string[MAX_LENGHT];
+			memset(command_string, 0, sizeof(command_string));
+			strcpy(command_string, tx_buff);
+			char *command = strtok(command_string, " ");
+			
+			if (strcmp(command, "/j") == 0) {
+				char channel_name[16];
+				strcpy(channel_name, strtok(NULL, "\n"));
+				if (channel_name[0] == '#' && strlen(channel_name) < 16) {
+					channel_t channel;
+					strcpy(channel.name, channel_name);
+					add_channel(&channel_list, &channel);
+					current_channel = &channel;
+					
+					strcpy(tx_buff, "JOIN ");
+					strcat(tx_buff, channel_name);
+					strcat(tx_buff, "\n");
+					write(data->socket, tx_buff, strlen(tx_buff));
+					continue;
+				} else {
+					printf("Illegal channel name\n");
+					continue;
+				}
+			}
+			
+			//printf("chan %d\n", channel_list.num_of_channels);
+			if (channel_list.num_of_channels > 0) {
+				memset(tx_buff, '\0', sizeof(tx_buff));
+				strcpy(tx_buff, "MSG ");
+				strcat(tx_buff, current_channel->name);
+				strcat(tx_buff, " ");
+				strcat(tx_buff, line);
+				strcat(tx_buff, "\n");
+				//printf("%s\n", tx_buff);
+				write(data->socket, tx_buff, strlen(tx_buff));
+				continue;
+			}	
+			
+			//printf("%s",tx_buff);
+			
 			write(data->socket, tx_buff, strlen(tx_buff));
-		}
-		memset(line, '\0', sizeof(line));
-		memset(tx_buff, '\0', sizeof(tx_buff));
+			
+		}	
+		
 		//sleep(1);
 	}
 }
@@ -89,28 +163,70 @@ void * send_message(void *ptr) {
 void * read_socket(void *ptr) {
 	char rx_buff[MAX_LENGHT];
 	char line[MAX_LENGHT];
-	int n;
+	int read_n = 0;
 	thdata *data;
 	data = (thdata *) ptr;
 	for(;;) {
-		n = read(data->socket, rx_buff, sizeof(rx_buff));
-		if (n > 0) {
+		//n = 0;
+		while (rx_buff[read_n - 1] != '\n') {
+			read_n += read(data->socket, rx_buff + read_n, sizeof(rx_buff));
+			
+			//sleep(2);
+			//printf("read_n %d rx_buf %s rx_buff@n-1 %d \n", read_n, rx_buff, rx_buff[read_n - 1]);
+			//printf("rx_buf %d", rx_buff[read_n - 1]);
+		}
+		if (read_n > 0) {
+			memset(line, '\0', sizeof(line));
 			char *command = strtok(rx_buff, " ");
 			if (strcmp(command, "MSG") == 0) {
-				strcpy(line, strtok(NULL, " "));
-				strcat(line, ": ");
-				strcat(line, strtok(NULL, "\n"));				
+				char *sender = strtok(NULL, " ");
+				char *channel_name = strcpy(line, strtok(NULL, " "));
+				if (strcmp(current_channel->name, channel_name) == 0) {
+					strcpy(line, sender);
+					strcat(line, "> ");
+					strcat(line, strtok(NULL, "\n"));
+				}	
+							
 			} else if (strcmp(command, "MOTD") == 0) {
-				strcpy(line , strtok(NULL, "\n"));
+				strcpy(line, COLOR_GREEN);
+				strcat(line , strtok(NULL, "\n"));
+				strcat(line, COLOR_RESET);
 				
+			} else if (strcmp(command, "CLOSE") == 0) {
+				strcpy(line, "Server closed connection: ");
+				strcat(line, strtok(NULL, "\n"));
+				
+			} else if (strcmp(command, "KILL") == 0) {
+				strcpy(line, COLOR_CYAN);
+				strcat(line, strtok(NULL, " "));
+				strcat(line, COLOR_RESET);
+				strcat(line, " was disconnected: ");
+				strcat(line, strtok(NULL, "\n"));
+				
+			} else if (strcmp(command, "LEAVE") == 0) {
+				strcpy(line, strtok(NULL, " "));
+				strcat(line, " left the channel ");
+				strcat(line, strtok(NULL, "\n"));
+				
+			} else if (strcmp(command, "NAMES") == 0) {
+				strcpy(line, "Users on ");
+				strcat(line, strtok(NULL, " "));
+				strcat(line, ": ");
+				strcat(line, strtok(NULL, "\n"));
+				
+			} else if (strcmp(command, "JOIN") == 0) {
+				strcpy(line, COLOR_CYAN);
+				strcat(line, strtok(NULL, " "));
+				strcat(line, COLOR_RESET);
+				strcat(line, " joined the channel");
 			} else {
 				strcpy(line, rx_buff);
 			}
 			printf("%s\n", line);
-			memset(line, '\0', sizeof(line));
+			
 		}
-		n = 0;
-		memset(rx_buff, '\0', sizeof(rx_buff));
+		read_n = 0;
+		memset(rx_buff, 0, sizeof(rx_buff));
 		//sleep(1);
 	}
 }	
@@ -150,4 +266,5 @@ int main(int argc, char **argv) {
 	
 	return 0;
 }
+
 
