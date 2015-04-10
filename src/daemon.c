f761ced4b031b8a87dd804c945219daad4cf8a2a
Antti Risteli <antti.risteli@aalto.fi>
2015-04-10 10:33:58 +0300

    Add PID file usage & handle SIGTERM properly

--- src/daemon.c
+++ src/daemon.c
@@ -11,6 +11,10 @@
 #include "daemon.h"
 #include "logging.h"
 
+void create_pid_file();
+void delete_pid_file();
+void sigterm_handler(int);
+
 // daemon technique adapted from 
 // http://www.linuxprofilm.com/articles/linux-daemon-howto.html
 // and http://stackoverflow.com/a/17955149
@@ -28,6 +32,7 @@ void init_daemon() {
     // ignore signals that we don't care about
     signal(SIGCHLD, SIG_IGN);
     signal(SIGHUP, SIG_IGN);
+    signal(SIGPIPE, SIG_IGN);
 
     // create a new SID for the child process 
     pid_t sid = setsid();
@@ -52,6 +57,10 @@ void init_daemon() {
     // make potential files created readable by all
     umask(0);
 
+    create_pid_file();
+    atexit(&delete_pid_file);
+    signal(SIGTERM, sigterm_handler);
+
     // reopen log file the take the umask into account
     log_reopen(); 
 
@@ -65,4 +74,37 @@ void init_daemon() {
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
+}
+
+int pid_file_exists() {
+    return access(PID_FILE_PATH, F_OK) != -1;
+}
+
+void delete_pid_file() {
+    // no way to handle the error in any way; this should only be calle dwhen the
+    // process is exitting
+    unlink(PID_FILE_PATH);
+}
+
+void sigterm_handler(int signum) {
+    signum = signum; // ignore unused parameter
+    delete_pid_file();
+}
+
+void create_pid_file() {
+    FILE *pid_file = fopen(PID_FILE_PATH, "w");
+    if (!pid_file) {
+        log_error("Could not create a PID file!\n"); 
+        exit(5);
+    }
+
+    if (fprintf(pid_file, "%ld\n", (long)getpid()) < 0) {
+        log_error("Could not write PID to file!\n"); 
+        exit(5);
+    }
+
+    if (fclose(pid_file) != 0) {
+        log_error("Could not close PID file!\n"); 
+        exit(5);
+    }
 }
